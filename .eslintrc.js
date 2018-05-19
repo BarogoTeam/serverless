1a2ea95d7bcf7f1e56a89da4260ca419f9c5b728
nicola <nicola@zoyi.co>
2018-05-19 18:52:31 +0900

    getAlarms API 작성

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:jsx-a11y/recommended"
+    ],
+
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import",
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single",
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "react/jsx-uses-vars": [
+            "error",
+        ],
+        "no-class-assign": [ // Too useful
+            "off"
+        ],
+        "no-unused-vars": [
+            "warn"
+        ],
+        "no-console": [
+            "warn",
+        ],
+        "comma-dangle": [
+            "warn",
+            "always-multiline"
+        ],
+        "eol-last": [
+            "error",
+        ]
+    },
+    "globals": {
+        "google": false,
+    }
+};
+
--- handler.js
+++ handler.js
@@ -1,3 +1,5 @@
+const DatabaseUtils = require("./DatabaseUtils").default;
+
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
@@ -13,4 +15,33 @@ module.exports.hello = (event, context, callback) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+
+module.exports.getAlarms = async function(event, context, callback) {
+  const db = await DatabaseUtils.connectMongoDB()
+
+  new Promise((resolve, reject) => {
+    db.collection('alarms').find({}).toArray((err, alarms) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(alarms)
+    })
+
+  }).then((alarms) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        alarms
+      }),
+    });
+  }).catch((e) => {
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        messages: [e.message]
+      }),
+    });
+  })
 };
