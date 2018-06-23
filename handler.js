d2d636eb284653e10ed7819b2a034eaf06ed8f94
cylee <cylee@cyleeui-MacBook-Pro.local>
2018-06-23 15:41:53 +0900

    open api 설정

--- handler.js
+++ handler.js
@@ -2,23 +2,7 @@ const DatabaseUtils = require("./DatabaseUtils").default;
 
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
-
-module.exports.getAlarms = async function(event, context, callback) {
+module.exports.getCinemas = async function(event, context, callback) {
   const db = await DatabaseUtils.connectMongoDB()
 
   new Promise((resolve, reject) => {
