181b131678ca12c97b8a87161c00acb165301e2d
Jaeyeon Lee <jylee@spb.ai>
2018-07-15 19:42:32 +0900

    Proxy 적용 및 getScreens 동작하는것 확인

--- handler.js
+++ handler.js
@@ -1,14 +1,14 @@
-const fetch = require('isomorphic-fetch');
+const fetch = require('fetch-with-proxy').default;
 const FormData = require('form-data');
 
 'use strict';
 
-module.exports.getCinemas = async (event, context, callback) => {
+module.exports.getCinemas = (event, context, callback) => {
   console.log(event.queryStringParameters);
   callback(null, event);
 };
 
-module.exports.getScreens = async (event, context, callback) => {
+module.exports.getScreens = (event, context, callback) => {
   // TODO(재연): event.queryStringParameters를 이용해서 cinemaId와 playDate 조작
   const formData = new FormData();
   formData.append('paramList', JSON.stringify({
@@ -16,27 +16,32 @@ module.exports.getScreens = async (event, context, callback) => {
     "channelType":"HO",
     "osType":"Firefox",
     "osVersion":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0",
-    "playDate":"2018-07-15",
+    "playDate":"2018-07-18",
     "cinemaID":"1|0001|1007,1|0001|1013", //NOTE(재연): UI상으로는 2개까지 제한이지만 API로는 무제한
     "representationMovieCode":""
   }));
 
   fetch(
-    '//www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx',
+    'http://www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx',
     {
       method: 'POST',
       body: formData,
     }
-  ).then(
-    response => response.json()
-  ).then(result => {
-    console.log(result);
+  ).then(response => {
+    return response.json()
+  }).then(result => {
     if (result.IsOK !== 'true') {
       throw new Error(JSON.stringify(result));
     }
-    console.log(result.PlaySeqs);
-    console.log(result.PlaySeqsHeader);
-  }).catch(e => {
+
+    // TODO(재연): 데이터 정제 로직 필요
+    return result;
+  }).then(body =>
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    })
+  ).catch(e => {
     console.error("Error on getScreens", e);
   })
 };
