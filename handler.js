97cdc777971e74305ce06017dfd7a8ef86bee98e
Jaeyeon Lee <jylee@spb.ai>
2018-07-21 12:13:37 +0900

    프록시 바꿈

--- handler.js
+++ handler.js
@@ -17,7 +17,7 @@ module.exports.getScreens = (event, context, callback) => {
     "channelType":"HO",
     "osType":"Firefox",
     "osVersion":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0",
-    "playDate":"2018-07-18",
+    "playDate":"2018-07-23",
     "cinemaID":"1|0001|1007,1|0001|1013", //NOTE(재연): UI상으로는 2개까지 제한이지만 API로는 무제한
     "representationMovieCode":""
   }));
