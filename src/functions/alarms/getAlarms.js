e2b3780588f7eddd9e6ce1ebb42c7f70a90ae145
logicalpsycho <37990015+logicalpsycho@users.noreply.github.com>
2018-09-15 16:51:11 +0900

    Login 되지 않은 경우 에러 처리

--- src/functions/alarms/getAlarms.js
+++ src/functions/alarms/getAlarms.js
@@ -1,10 +1,26 @@
 import jwt from 'jsonwebtoken';
+import _ from 'lodash';
+
 import DatabaseUtils from '../../utils/DatabaseUtils';
 
 export default async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const token = event.headers.Authorization.split(' ')[1]; // TODO: 재연이가 고쳐줄것임
-  const email = jwt.verify(token, 'secret').data;
+
+  if (!_.get(event, 'headers.Authorization')) {
+    return callback(null, {
+      statusCode: 401,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+        errorMessage: 'Login is required',
+      }),
+    });
+  }
+  const token = event.headers.Authorization.split(' ')[1];
+
+  const email = jwt.verify(token, 'secret').data; // TODO: jwt가 valid하지 않은 경우 처리
   const query = {
     email,
   };
@@ -29,5 +45,5 @@ export default async (event, context, callback) => {
       })
   );
 
-  callback(null, await response);
+  return callback(null, await response);
 };
--- src/functions/alarms/postAlarms.js
+++ src/functions/alarms/postAlarms.js
@@ -1,11 +1,27 @@
 import jwt from 'jsonwebtoken';
+import _ from 'lodash';
+
 import DatabaseUtils from '../../utils/DatabaseUtils';
 
 export default async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
+
+  if (!_.get(event, 'headers.Authorization')) {
+    return callback(null, {
+      statusCode: 401,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+        errorMessage: 'Login is required',
+      }),
+    });
+  }
+
+  const token = event.headers.Authorization.split(' ')[1];
   const alarm = JSON.parse(event.body);
-  const token = event.headers.Authorization.split(' ')[1]; // TODO: 재연이가 고쳐줄것임
-  alarm.email = jwt.verify(token, 'secret').data;
+  alarm.email = jwt.verify(token, 'secret').data; // TODO: jwt가 valid하지 않은 경우 처리
 
   const response = new Promise(resolve => {
     DatabaseUtils.connectMongoDB().then(db =>
@@ -23,7 +39,7 @@ export default async (event, context, callback) => {
     });
   });
 
-  callback(null, await response);
+  return callback(null, await response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
