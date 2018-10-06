fdacab9d261bac1cac238c4937118ed36faf148f
cylee <cylee@cyleeui-MacBook-Pro.local>
2018-10-06 16:59:51 +0900

    JWT error 처리 getAlarms

--- src/functions/alarms/getAlarms.js
+++ src/functions/alarms/getAlarms.js
@@ -20,30 +20,43 @@ export default async (event, context, callback) => {
   }
   const token = event.headers.Authorization.split(' ')[1];
 
-  const email = jwt.verify(token, 'secret').data; // TODO: jwt가 valid하지 않은 경우 처리
-  const query = {
-    email,
-  };
+  const response = new Promise(resolve => {
+    try {
+      const email = jwt.verify(token, 'secret').data; // TODO: jwt가 valid하지 않은 경우 처리
+      const query = {
+        email,
+      };
 
-  const response = new Promise(resolve =>
-    DatabaseUtils.connectMongoDB()
-      .then(db =>
-        db
-          .collection('alarms')
-          .find(query)
-          .toArray()
-      )
-      .then(alarms => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-            'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-          },
-          body: JSON.stringify(alarms),
+      DatabaseUtils.connectMongoDB()
+        .then(db =>
+          db
+            .collection('alarms')
+            .find(query)
+            .toArray()
+        )
+        .then(alarms => {
+          resolve({
+            statusCode: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+              'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+            },
+            body: JSON.stringify(alarms),
+          });
         });
-      })
-  );
+    } catch (e) {
+      resolve({
+        statusCode: 401,
+        headers: {
+          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+          'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify({
+          errorMessage: e.message,
+        }),
+      });
+    }
+  });
 
   return callback(null, await response);
 };
